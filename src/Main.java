d0ac7ecae0c4bd1042777b9662c729a499f206a4
Expo <crdailey01@gmail.com>
2023-10-12 18:37:56 -0700

    annootated

--- src/Main.java
+++ src/Main.java
@@ -13,62 +13,64 @@ public class Main {
 
 // i was right with queue idea but need an O(1) solution since if a key is in the queue and we call it
 // again, need to remove it frmo its position and add to end of queue, costing O(n)
-class ListNode {
+class ListNode { // fields for ListNode class
     int key;
     int val;
     ListNode next;
     ListNode prev;
 
-    public ListNode(int key, int val) {
+    public ListNode(int key, int val) { // constructor
         this.key = key;
         this.val = val;
     }
 }
 
-class LRUCache {
+class LRUCache { // cache class will contains a doubly linked List and a Map with nodes as values for O(1) retrieval/replace
+    // trrying to think of this as like a super data structure of the two combined
+    // fields for cache class
     int capacity;
     Map<Integer, ListNode> dic;
     ListNode head;
     ListNode tail;
 
-    public LRUCache(int capacity) {
+    public LRUCache(int capacity) {  // constructor for cache where capacity defined but all else empty
         this.capacity = capacity;
         dic = new HashMap<>();
-        head = new ListNode(-1, -1);
-        tail = new ListNode(-1, -1);
+        head = new ListNode(-1, -1); // this is a dummy/sentinal node such that when DLL is empty or we deleted only
+        tail = new ListNode(-1, -1); // remaining node, that the head and tail pointers will never equal null
         head.next = tail;
         tail.prev = head;
     }
 
     public int get(int key) {
-        if (!dic.containsKey(key)) {
+        if (!dic.containsKey(key)) { // no key -> -1
             return -1;
         }
 
-        ListNode node = dic.get(key);
-        remove(node);
-        add(node);
+        ListNode node = dic.get(key); // if key
+        remove(node); // remove prior instance from DLL
+        add(node); // add instance at tail of DLL since it was most recently used
         return node.val;
     }
 
     public void put(int key, int value) {
-        if (dic.containsKey(key)) {
-            ListNode oldNode = dic.get(key);
-            remove(oldNode);
+        if (dic.containsKey(key)) { // if exists
+            ListNode oldNode = dic.get(key); // get node
+            remove(oldNode); // remove it from DLL
         }
 
-        ListNode node = new ListNode(key, value);
-        dic.put(key, node);
+        ListNode node = new ListNode(key, value); // add node to end of DLL after defining
+        dic.put(key, node); // note that the node key is the same key in the HashMap to link the two data structures
         add(node);
 
-        if (dic.size() > capacity) {
-            ListNode nodeToDelete = head.next;
+        if (dic.size() > capacity) { // if capacity breached,
+            ListNode nodeToDelete = head.next; // define and delete true head, not sentinel
             remove(nodeToDelete);
             dic.remove(nodeToDelete.key);
         }
     }
 
-    public void add(ListNode node) {
+    private void add(ListNode node) { // add new node to real end between prior end and sentinel end
         ListNode previousEnd = tail.prev;
         previousEnd.next = node;
         node.prev = previousEnd;
@@ -76,11 +78,16 @@ class LRUCache {
         tail.prev = node;
     }
 
-    public void remove(ListNode node) {
+    private void remove(ListNode node) { // the add and remove methods were public, but that's bad encapsulation IMO.
         node.prev.next = node.next;
         node.next.prev = node.prev;
+        // this didn't actually remove the node though and it won't be garbage collected since its still linked
+        // by its pointers even though nothing points to it. over time, this will use way too much memory.
+        node.prev = null;
+        node.next = null;
     }
 }
+
 
 /**
  * Your LRUCache object will be instantiated and called as such:
